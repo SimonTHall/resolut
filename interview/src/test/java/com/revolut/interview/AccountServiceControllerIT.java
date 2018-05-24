1b4735aa82846a4ad61fca2fad650e049609144a
SimonTHall <39503520+SimonTHall@users.noreply.github.com>
2018-05-24 19:52:51 +0100

    Added maven-compiler-plugin for java 8 and added concurrency test

--- interview/src/test/java/com/revolut/interview/AccountServiceControllerIT.java
+++ interview/src/test/java/com/revolut/interview/AccountServiceControllerIT.java
@@ -7,6 +7,10 @@ import static com.revolut.interview.TransactionType.DEBIT;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
@@ -58,6 +62,40 @@ public class AccountServiceControllerIT extends JerseyTest {
         assertThat(response.readEntity(String.class)).isEqualTo("Initial deposit must be positive!");
     }
 
+    @Test
+    public void testConcurrentTransfers() throws InterruptedException {
+
+        // given    
+        for (int i=0; i<200; i++) {
+            addAccountAndCheck("Joe Smith", 0, 1000);
+            addAccountAndCheck("Fred Blogs", 0, 1000);            
+        }
+
+        Account joeSmith = addAccountAndCheck("Joe Smith", 0, 1000);
+        Account fredBlogs = addAccountAndCheck("Fred Blogs", 0, 1000);
+
+        // when    
+        ExecutorService service = Executors.newFixedThreadPool(8);    
+        for (int i = 0; i < 1000; i++) {
+            service.submit(() -> {    
+                if (ThreadLocalRandom.current().nextBoolean()) {
+                    transferFunds(joeSmith.getNumber(), fredBlogs.getNumber(), 1000);
+                } else {    
+                    transferFunds(fredBlogs.getNumber(), joeSmith.getNumber(), 1000);    
+                }    
+            });    
+        }
+    
+        service.shutdown();    
+        service.awaitTermination(10, TimeUnit.MINUTES);
+    
+        // then    
+        assertThat(getAccount(joeSmith.getNumber()).getCurrentBalance()).isNotNegative();    
+        assertThat(getAccount(joeSmith.getNumber()).getCurrentBalance()).isLessThanOrEqualTo(BigDecimal.valueOf(2000));    
+        assertThat(getAccount(fredBlogs.getNumber()).getCurrentBalance()).isNotNegative();    
+        assertThat(getAccount(fredBlogs.getNumber()).getCurrentBalance()).isLessThanOrEqualTo(BigDecimal.valueOf(2000));
+
+    }
     private Account getAccount(Integer accountNo) {
         return target("accounts/" + accountNo).request().get(Account.class);                
     }
@@ -68,10 +106,11 @@ public class AccountServiceControllerIT extends JerseyTest {
         target("accounts/transfer").request().post(requestEntity);        
     }
 
-    private void addAccountAndCheck(String name, Integer overDraftLimit, Integer initialDeposit) {
+    private Account addAccountAndCheck(String name, Integer overDraftLimit, Integer initialDeposit) {
         Response response = addAccount(name, overDraftLimit, initialDeposit);
         Account newAccount = response.readEntity(Account.class);
         assertThat(newAccount.getCustomerName()).isEqualTo(name);
+        return newAccount;
     }
 
     private Response addAccount(String name, Integer overDraftLimit, Integer initialDeposit) {
